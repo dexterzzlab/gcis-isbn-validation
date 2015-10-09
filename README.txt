7ac5c8a2fcd147141adf0b24fe3606a065d7e7d9
Gerald Manipon <pymonger@gmail.com>
2015-10-09 16:21:34 +0000

    cleaned up, updated readme, and added sample run

--- README.txt
+++ README.txt
@@ -4,16 +4,29 @@ Purpose
 This set of scripts is designed to validate the isbn information we have for all the books in GCIS
 
 Process
-(should be wrapped up in isbn_script.sh)
-1. Use python get_books.py to retrieve JSON dumps from all books on data.globalchange.gov
-2. Use python normalize_isbn.py to convert book isbn to isbn13 format, if it exists
-3. Use python get_loc_xml.py to check if isbns exist on Library of Congress database
+1. Use 'python get_books.py [GCIS book JSON endpoint]' to retrieve json dumps for all books from the GCIS book JSON endpoint
+2. Use 'python normalize_isbn.py [top level GCIS script-created directory]' to normalize all the JSON files to ISBN13 if an ISBN13 version exists
+3. Use 'python get_loc_xml.py [top level GCIS script-created directory]' to check existing isbn entries in json files against Library of Congress Database
 
-This process generates folders with the following:
-Original JSON files from data.globalchange.gov
-Normalized JSON files (Separated to ISBN13, non-ISBN13, and no ISBN subfolders)
-XML files for matched ISBNs from Library of Congress (Separated to ISBN13, non-ISBN13, and problem file subfolders)
+At this point, the following directories will be created:
 
+/gcis-[DATE]T[TIME] (This is in ISO8601 spec)
+--> book
+--> book-loc
+--> book-normalized
+--> book-to-fix
+--> book-to-ingest
+
+
+These folders contain the following:
+[ book ] : Original JSON files from data.globalchange.gov
+[ book-loc ] : XML files for matched ISBNs from Library of Congress (Separated into isbn-13-match, isbn-other-match, and isbn-problem subfolders)
+[ book-normalized ] : Normalized JSON files (Separated into isbn-13, isbn-none, and isbn-other subfolders)
+[ book-to-fix ] : Books where ISBNs did not match Library of Congress database. These need to be manually handled.
+[ book-to-ingest ] : JSON files of the cleaned/normalized and verified books that are ready to ingest into GCIS
+
+To ingest this data into GCIS elastic search, you simply run the following:
+'python gcis_es_crawler.py gcis-[DATE]T[TIME]/book-to-ingest'
 
 Notes:
 Looks like there are only 3 isbn files missing
--- get_loc_xml.py
+++ get_loc_xml.py
@@ -32,9 +32,11 @@ if len(sys.argv) > 1:
 
             valid13_dir = norm_dir + "isbn-13/"
 
+            norm_prob_dir = norm_dir + "isbn-none/"
+
             loc_xml_dir = top_dir + "book-loc/"
 
-
+            to_fix_dir = top_dir + "book-to-fix/"
 
             #valid13_dir = top_dir+"normalization_output/isbn13_book/"
 
@@ -51,10 +53,10 @@ if len(sys.argv) > 1:
             #problem_dir_other = loc_xml_dir +  "problem_other/"
 
 
+            
 
-            log = open(top_dir+"get_loc_xml_log.txt", 'w')
 
-
+            json_to_ingest = top_dir + "book-to-ingest/"
 
             if not os.path.isdir(loc_xml_dir):
                 os.makedirs(loc_xml_dir)
@@ -64,54 +66,87 @@ if len(sys.argv) > 1:
                 os.makedirs(problem_dir)
             if not os.path.isdir(match_dir_other):
                 os.makedirs(match_dir_other)
+            if not os.path.isdir(json_to_ingest):
+                os.makedirs(json_to_ingest)
+            if not os.path.isdir(to_fix_dir):
+                os.makedirs(to_fix_dir)
             #if not os.path.isdir(problem_dir_other):
             #    os.makedirs(problem_dir_other)
+            log = open(loc_xml_dir+"get_loc_xml_log.txt", 'w')
+
 
             problem_isbn_counter = 0
             isbn13_file_counter = 0
+
             for (root, dirs, files) in os.walk(valid13_dir):
                 for f in files:
+                    jsonName = f
                     fname = f[:-5]+".xml"
                     with open(valid13_dir+f) as item:
                         json_item = json.load(item)
                         book_isbn = json_item['isbn']
                         loc_xml = requests.get('http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&query=bath.isbn={}&maximumRecords=1&recordSchema=mods'.format(book_isbn))
-                        loc_xml = loc_xml.text
-                        if loc_xml != None:
+                        if loc_xml.raise_for_status() == None:
+                           loc_xml = loc_xml.text
                            with open(match_dir_13+str(fname), 'w') as item:
                                 item.write(loc_xml)
                                 isbn13_file_counter += 1
+                            
+                           with open(json_to_ingest+str(jsonName), 'w') as jsonItem:
+                                jsonItem.write(json.dumps(json_item, sort_keys=True, indent=4, separators=(',',': ')))
                         else:
                             with open(problem_dir+str(fname), 'w') as item:
                                 problem_isbn_counter+=1
                                 log.write(json_item['isbn']+"\n")
-                                item.write(json_item)
-                    time.sleep(10)	
+                                item.write(json.dumps(json_item, sort_keys=True, indent=4, separators=(',',': ')))
+                    time.sleep(1)	
 
             non_isbn13_counter = 0
             for (root, dirs, files) in os.walk(non13_dir):
                 for f in files:
+                    jsonName = f
                     fname = f[:-5]+".xml"
                     with open(non13_dir+f) as item:
                         json_item = json.load(item)
                         book_isbn = json_item['isbn']
                         loc_xml = requests.get('http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&query=bath.isbn={}&maximumRecords=1&recordSchema=mods'.format(book_isbn))
-                        loc_xml = loc_xml.text
-                        if loc_xml != None:
+                        
+                        if loc_xml.raise_for_status() == None:
+                           loc_xml = loc_xml.text   
                            with open(match_dir_other+str(fname), 'w') as item:
                                 item.write(loc_xml)
                                 non_isbn13_counter += 1
+                           with open(json_to_ingest+str(jsonName), 'w') as jsonItem:
+                               jsonItem.write(json.dumps(json_item, sort_keys=True, indent=4, separators=(',',': ')))
                         else:
                             with open(problem_dir+str(fname), 'w') as item:
-                                item.write(json_item)
+                                item.write(json.dumps(json_item, sort_keys=True, indent=4, separators=(',',': ')))
                                 problem_isbn_counter+=1
                                 log.write(json_item['isbn']+"\n")
 
-             
+
+           
+            for(root,dirs,files) in os.walk(norm_prob_dir):
+                for f in files:
+                    with open(norm_prob_dir+f) as item:
+                        json_item = json.load(item)
+
+                        with open(problem_dir + str(f), 'w') as jsonItem:
+                            jsonItem.write(json.dumps(json_item, sort_keys=True, indent=4, separators=(',', ': ')))
+                            problem_isbn_counter += 1
+
+            for(root,dirs,files) in os.walk(problem_dir):
+                for f in files:
+                    with open(problem_dir+f) as item:
+                        json_item = json.load(item)
+                        with open(to_fix_dir + str(f), 'w') as jsonItem:
+                            jsonItem.write(json.dumps(json_item, sort_keys=True, indent=4, separators=(',', ': ')))
             log.write("isbn 13 match: %s" % isbn13_file_counter + "\n")
             log.write("non isbn13 match: %s" % non_isbn13_counter + "\n")
             log.write("problem files: %s" %problem_isbn_counter)
+
         else:
+
             print "ISBNs have not been normalized yet. Please run normalize_isbn.py first"
     else:
         print "Path parameter for GCIS top level directory is not valid"
